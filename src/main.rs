a0d5b9f35beefb90315cb272499ca8847450cd9f
cetra3 <peter@parashift.com.au>
2016-10-06 19:07:20 +1030

    Include 'modules' download

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,249 @@
 extern crate zip;
 extern crate yaml_rust;
+extern crate clap;
+extern crate crypto;
 
-use yaml_rust::{yaml,YamlLoader};
+#[macro_use]
+extern crate hyper;
+
+use yaml_rust::{Yaml,YamlLoader};
 use zip::read::{ZipArchive, ZipFile};
 use std::fs::{self, File};
 use std::path::Path;
 use std::io::{copy, Write, Read, BufReader, BufRead, Error, ErrorKind, Result};
 use std::collections::{HashMap, HashSet};
-use std::env;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+use std::fmt;
+
+use hyper::Client;
+use hyper::status::StatusCode;
+
+use clap::{Arg, App};
+
+header! { (Token, "TOKEN") => [String] }
+
+struct AmpModule {
+    vendor: String,
+
+    name: String,
+
+    version: String,
+
+    module_type: String
+
+}
+
+impl AmpModule{
+    fn new(module: &str, module_type:&str) -> AmpModule {
+        let parts: Vec<&str> = module.split(":").collect();
+
+        AmpModule{
+            vendor: String::from(parts[0]),
+            name: String::from(parts[1]),
+            version: String::from(parts[2]),
+            module_type: String::from(module_type)
+        }
+
+    }
+}
+
+impl fmt::Display for AmpModule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}:{}:{}", self.vendor, self.name, self.version, self.module_type)
+    }
+}
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Paramp")
+        .version("1.0.0")
+        .author("Peter Lesty <peter@parashift.com.au>")
+        .about("Generate an Alfresco deployment with modules")
+        .arg(Arg::with_name("yaml_file")
+            .help("Source Yaml file for modules")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("output_dir")
+            .help("Target Directory")
+            .index(2))
+        .arg(Arg::with_name("token")
+            .help("Override Config Token")
+            .short("t")
+            .takes_value(true))
+        .arg(Arg::with_name("type")
+            .help("Module Type, either 'repo' or 'share'")
+            .short("m")
+            .takes_value(true))
+        .arg(Arg::with_name("url")
+            .help("URL of Packages server")
+            .short("u")
+            .takes_value(true))
+        .get_matches();
 
-    match args.len() {
-        2 => generate_entirely_from_yaml(&args[1]),
-        3 => generate_from_yaml_with_output(&args[1], &args[2]),
-        _ => {
-            println!("PARAMP: Generate an Alfresco deployment with modules");
-            println!("Usage: {} source.yaml <target_dir>", args[0])
+    let input_file = matches.value_of("yaml_file").unwrap();
+
+    let yaml = get_yaml(input_file);
+
+    let output_dir = matches.value_of("output_dir")
+        .map(|dir| String::from(dir))
+        .unwrap_or_else(|| get_yaml_string(&yaml, "output_dir").expect("Could not get target directory from YAML file"));
+
+    let token: Option<String> = matches.value_of("token")
+        .map(|token| String::from(token))
+        .or(get_yaml_string(&yaml, "token"));
+
+    let module_type: Option<String> = matches.value_of("type")
+        .map(|token| String::from(token))
+        .or(get_yaml_string(&yaml, "type"));
+
+
+    let mut files = Vec::new();
+
+    if module_type.is_some() && token.is_some() {
+
+        let url: String = matches.value_of("url")
+            .map(|token| String::from(token))
+            .or(get_yaml_string(&yaml, "url"))
+            .unwrap_or(String::from("https://repo.parashift.com.au"));
+
+        let modules = get_yaml_string_list(&yaml, "modules");
+
+        files.append(&mut download_files(&modules, &module_type.unwrap(), &token.unwrap(), &url));
+
+    }
+
+    match fs::remove_dir_all(&output_dir) {
+        Ok(_) => {
+            println!("Clearing dir: {}", output_dir);
+        },
+        _ => {}
+    }
+
+    files.append(&mut get_yaml_string_list(&yaml, "files"));
+
+    output_files(files, &output_dir);
+
+}
+
+fn download_files(modules: &Vec<String>, module_type: &str, token: &str, url: &str) -> Vec<String> {
+
+    fs::create_dir_all(".ampcache").unwrap();
+
+    let mut return_files = Vec::new();
+
+    let client = Client::new();
+
+    for module in modules.into_iter().map(|module| AmpModule::new(module, module_type)) {
+
+        let file_name = format!(".ampcache/{}-{}-{}-{}.amp", module.vendor, module.name, module.version, module.module_type);
+
+        let submit_url = format!("{}/module/{}/{}/{}/{}", url, module.vendor, module.name, module.version, module.module_type);
+
+        let mut response = client.get(&submit_url)
+            .send()
+            .unwrap();
+
+        match response.status {
+            StatusCode::Ok => {
+                let mut checksum = String::new();
+
+                response.read_to_string(&mut checksum).expect("Could not read response");
+
+                if checksum.len() > 0 {
+
+                    let local_file = resolve_file(&file_name);
+
+                    if !local_file.is_ok() || !compare_checksum(local_file.unwrap(), checksum) {
+
+                        let mut new_file = create_file_and_dirs(&file_name).unwrap();
+
+                        let mut file_dl = client.get(&*format!("{}.amp", submit_url))
+                            .header(Token(String::from(token)))
+                            .send()
+                            .unwrap();
+
+                        match file_dl.status {
+                            StatusCode::Ok => {
+                                println!("Downloading '{}'", module);
+                                copy(&mut file_dl, &mut new_file).expect("Error saving file!");
+                            },
+                            status => panic!("Could not get '{}' ({})", module, status)
+                        }
+
+                    }
+
+                    return_files.push(file_name);
+                } else {
+                    panic!("Could not get '{}' (Invalid Server Checksum)", module)
+                }
+
+            },
+            StatusCode::SeeOther => {
+                println!("Skipping module '{}' (No '{}' component)", module, module_type)
+            }
+            status => panic!("Could not get '{}' ({})", module, status)
         }
     }
-}
 
-fn generate_entirely_from_yaml(input_file: &str) {
-
-    match read_file(resolve_file(input_file).unwrap()) {
-        Ok(contents) => {
-            let input = YamlLoader::load_from_str(&contents).unwrap();
-
-            match &input[0]["output_dir"] {
-                &yaml::Yaml::String(ref output_dir) => {
-
-                    generate_from_yaml_with_output(input_file, output_dir);
-
-                }
-                _ => { println!("Could not find an output_dir property in input yaml file, specify an output directory with `output_dir` on the command line or within the yaml file");}
-            }
-
-        },
-        _ => {}
-    }
-
+    return return_files
 
 }
 
-fn generate_from_yaml_with_output(input_file: &str, output_dir: &str) {
+fn compare_checksum(mut file: File, checksum: String) -> bool {
 
-    match fs::remove_dir_all(output_dir) {
-        Ok(_) => {
-            println!("   Clearing dir: {}", output_dir);
-        },
-        _ => {}
+    let mut sh = Md5::new();
+
+    let mut buf = Vec::new();
+
+    file.read_to_end(&mut buf).unwrap();
+
+    sh.input(&buf);
+
+    let file_sum = sh.result_str();
+
+    return file_sum == checksum;
+
+}
+
+fn get_yaml_string_list(yaml: &Yaml, value: &str) -> Vec<String> {
+    match yaml[value] {
+        Yaml::Array(ref array) => {
+            array.into_iter().map(|value| String::from(value.as_str().unwrap())).collect()
+        }
+        _ => Vec::new()
     }
 
+}
+
+fn get_yaml_string(yaml: &Yaml, value: &str) -> Option<String> {
+    match yaml[value] {
+        Yaml::String(ref yaml_value) => Some(yaml_value.clone()),
+        _ => None
+    }
+}
+
+fn get_yaml(input_file: &str) -> Yaml {
     match read_file(resolve_file(input_file).unwrap()) {
-        Ok(contents) => {
-            let input = YamlLoader::load_from_str(&contents).unwrap();
+        Ok(contents) => YamlLoader::load_from_str(&contents).unwrap()[0].clone(),
+        Err(_) => panic!("Could not read file")
+    }
+}
 
-            match &input[0]["files"] {
-                &yaml::Yaml::Array(ref v) => {
-                    for amp in v {
+fn output_files(input_files: Vec<String>, output_dir: &str) {
 
-                        let amp = amp.as_str().unwrap();
-
-                        println!("Extracting file: {}", amp);
-
-                        generate_output(amp, output_dir);
-                    }
-                },
-                _ => {}
-            }
-
-        },
-        _ => {}
+    for file in input_files.iter() {
+        generate_output(&file, output_dir);
     }
 
 }
 
 fn generate_output(input_file: &str, output_dir: &str) {
 
-    let file = File::open(input_file).unwrap();
+    println!("Extracting file: {}", input_file);
+
+    let file = resolve_file(input_file).unwrap();
 
     let mut archive = ZipArchive::new(file).unwrap();
 
@@ -249,6 +412,6 @@ fn resolve_file(search_path: &str) -> Result<File> {
         true => File::open(&path),
         false =>
                 Err(Error::new(ErrorKind::NotFound,
-                              "the file cannot be found"))
+                              format!("the file at {} cannot be found", search_path)))
     }
 }
